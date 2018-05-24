b937c153ad4e10da68870016d0db09ebaa1322b4
Laurence Cook <lwc24@cam.ac.uk>
2018-05-24 18:17:35 +0100

    Added comments and changed names

--- run_optimization.py
+++ run_optimization.py
@@ -6,6 +6,11 @@ import src.geninforeuse as gir
 import src.setup as setup
 
 def deterministic_objective(x, u):
+######################################################################
+# Change this to be the objective function for a new model
+# It takes two inputs: the design variables as the vector x, and
+# uncertain input parameters as the vector u
+######################################################################
 
     qrosen = ((2-x[0])**2 + 2*(x[1] - x[0]**2)**2)
 
@@ -19,6 +24,11 @@ def deterministic_objective(x, u):
 
 
 def deterministic_constraint(x, u):
+######################################################################
+# Change this to be a constraint function for a new model
+# It takes two inputs: the design variables as the vector x, and
+# uncertain input parameters as the vector u
+######################################################################
 
     qu = 0.0
     for ii, ui in enumerate(u):
@@ -26,6 +36,10 @@ def deterministic_constraint(x, u):
         qu += (1./(ii+1.)) * (0.2 + 0.2*(s+1)**2) * ui
 
     return 2.0 - np.exp(qu)
+
+######################################################################
+# Define any more constraints as required
+######################################################################
 
 
 def runopt(IRobj, IRconstr, x0, lb, ub):
@@ -60,27 +74,45 @@ def main(case='Test'):
     lb = [-2, -2]  # Lower bounds on design space
     ub = [2, 2]  # Lower bounds on design space
 
-    x0 = [lb[i] + (ub[i]-lb[i]) for i in range(nx)]
+    x0 = [lb[i] + (ub[i]-lb[i]) for i in range(nx)] # Initial design point
 
-    n_boot = 2000
-    n_init = 50
+    n_boot = 2000  # Number of bootstrap resampels to perform
+    n_init = 50  # Initial number of samples used at each iteration
+
+    ######################################################################
+    # Change these required variances for the problem at hand.
+    # Var_obj is required variance by an estimator of the objective
+    # Var_constraint is required variance by an estimator of the constraint
+    ######################################################################
     var_obj = 50e-4
     var_constr = 40e-4
 
-    targ = lambda h: setup.custom_target(h, q0=0, q1=-4, p=6)
+    ######################################################################
+    # Define the statistical estimators of interest here
+    ######################################################################
+    def obj_estimator(samples):
+        targ = lambda h: setup.custom_target(h, q0=0, q1=-4, p=6)
+        return setup.hm_estimator(samples, targ=targ),
+
+    def constr_estimator(samples)
+        return setup.quantile_estimator(samples, 0.9),
 
     ## Using information reuse
     theIRobj = gir.GeneralInformationReuse(fobj=deterministic_objective,
               udim=nu, xdim=nx,
-              estimator=lambda vq: setup.hm_estimator(vq, targ=targ),
+              estimator=obj_estimator,
               n_init=n_init, n_boot=n_boot, var_req=var_obj, check=False,
               verbose=True, log_name=case+'_log_IR_objective')
 
     theIRconstr = gir.GeneralInformationReuse(fobj=deterministic_constraint,
               udim=nu, xdim=nx,
-              estimator=lambda vq: setup.quantile_estimator(vq, 0.9),
+              estimator=constr_estimator
               n_init=n_init, n_boot=n_boot, var_req=var_constr, check=False,
               verbose=True, log_name=case+'_log_IR_constraint')
+
+    ######################################################################
+    # Define additional information reuse objects for additional constraints
+    ######################################################################
 
     runopt(theIRobj, theIRconstr, x0, lb, ub)
 
